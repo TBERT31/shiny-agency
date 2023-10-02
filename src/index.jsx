7748d5808cc342e44362ffab77941e005ebbe957
TBERT31 <78256617+TBERT31@users.noreply.github.com>
2023-10-02 13:49:26 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -34,17 +34,18 @@
 // Real file
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/'
- 
+import Survey from './pages/Survey/'
+
 ReactDOM.render(
-    <React.StrictMode>
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-            <Home />
-        </Router>
-    </React.StrictMode>,
-document.getElementById('root')
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/survey" element={<Survey />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 )
