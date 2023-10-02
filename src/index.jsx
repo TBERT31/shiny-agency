2fc91ae9eb07726251f426b7c8c0f1518fa96dcb
TBERT31 <78256617+TBERT31@users.noreply.github.com>
2023-10-02 13:48:42 +0200

    Update index.jsx
    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
+// // Init v18
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
 // import './index.css';
@@ -15,15 +16,35 @@
 // // to log results (for example: reportWebVitals(console.log))
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './pages/Home/App';
+
+// // Init v17
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+//import './index.css';
+//import App from './pages/Home/App';
 
 
+//ReactDOM.render(
+  //<React.StrictMode>
+   // <App />
+  //</React.StrictMode>,
+ // document.getElementById('root')
+//);
+
+// Real file
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/'
+ 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+            <Home />
+        </Router>
+    </React.StrictMode>,
+document.getElementById('root')
+)
